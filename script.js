0e399495dd9e5d3e3e2526c11547d843fd269e59
alicia16489 <kawasaki1>
2013-10-23 20:19:12 +0200

    merge texture with features

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ function get_random_color() {
 
     var CMB = {};
     (function (CMB) {
-        var globals, texture, images, current_lvl, hero, movableItem;
+        var globals, texture, images, current_lvl, hero, item, load, game;
+
+        load = 0;
 
         globals = [
             {
@@ -32,25 +34,37 @@ function get_random_color() {
                     144,143,142,141,140,139,138],
                 hero : [175],
                 rock : [202,178,154,188],
-                bomb : [],
-                hard_rock : [],
+                bomb : [151],
+                hard_rock : [176],
                 button : [176],
                 toggle_rock : [201,189,177,165,153],
                 toggle_rock_link : {176:[201,189,177,165,153]},
                 goal : []
             }];
 
-        images = {
+        /*images = {
             'bordure' : "#000",
             'hero' : "#555",
             'rock' : "#999",
             'floor' : "#ff0000",
-            'bomb' : "",
+            'bomb' : "EEA443",
             'hard_rock' : "",
             'button' : "#00ff00",
             'toggle_rock_on' : "0000ff",
             'toggle_rock_off' : "F6A1E1",
             'goal' : ""
+        };*/
+
+        images = {
+            'floor' : "./images/floor/floor0.png",
+            'out' : "./images/floor/out.png",
+            "hero_bot" : "./images/hero/hero_bot.png",
+            "rock" : "./images/items/rock.png",
+            "toggle_rock_on" : "./images/items/toggle_rock_on.png",
+            "toggle_rock_off" : "./images/items/toggle_rock_off.png",
+            "bomb" : "./images/items/bomb.png",
+            "button_off" : "./images/items/button_off.png",
+            "button_on" : "./images/items/button_on.png"
         };
 
         texture = function (x,y,type) {
@@ -67,20 +81,41 @@ function get_random_color() {
             this.on = on;
         };
 
-        movableItem = function (x,y,type,on) {
+        item = function (x,y,type,on,prev) {
             this.x = x;
             this.y = y;
             this.type = type;
             this.on = on;
+            this.prev = prev;
         };
 
         CMB.game = {
+            loadImages : function(){
+              var img;
+
+              for(var key in images){
+                  img = new Image();
+                  img.src = images[key];
+                  images[key] = img;
+                  img.onload = CMB.game.preload;
+              }
+            },
+            preload : function(){
+                var length = Object.keys(images).length;
+
+                load++;
+                console.log(images,load);
+                if(load === length){
+                  CMB.game.draw();
+                }
+
+            },
             init : function (lvl) {
                 var lvl = lvl - 1;
                 current_lvl = lvl;
                 globals[lvl].$canvas = $("#canvas:first");
                 globals[lvl].context = globals[lvl].$canvas[0].getContext('2d');
-                CMB.game.draw();
+                CMB.game.loadImages();
             },
             draw : function () {
                 var lvl = current_lvl;
@@ -89,38 +124,42 @@ function get_random_color() {
                 for (i; i > 0; i--) {
 
                     var type = "";
+                    globals[lvl].context.drawImage(images["floor"],(row * 50),(col * 50));
 
                     if ($.inArray(i,globals[lvl].bordures) != -1) {
-                        globals[lvl].context.fillStyle = images.bordure;
+                        globals[lvl].context.drawImage(images["out"],(row * 50),(col * 50));
                         type = "bordure";
                     }
                     else if($.inArray(i,globals[lvl].hero) != -1){
-                        globals[lvl].context.fillStyle = images.hero;
+                        globals[lvl].context.drawImage(images["hero_bot"],(row * 50),(col * 50));
                         type = "hero";
                     }
                     else if($.inArray(i,globals[lvl].rock) != -1){
-                        globals[lvl].context.fillStyle = images.rock;
+                        globals[lvl].context.drawImage(images["rock"],(row * 50),(col * 50));
                         type = "rock";
                     }
                     else if($.inArray(i,globals[lvl].toggle_rock) != -1){
-                        console.log("here");
-                        globals[lvl].context.fillStyle = images.toggle_rock_on;
-                        type = "toggle_rock_on";
+                        globals[lvl].context.drawImage(images["toggle_rock_off"],(row * 50),(col * 50));
+                        type = "toggle_rock_off";
                     }
                     else if($.inArray(i,globals[lvl].button) != -1){
-                        globals[lvl].context.fillStyle = images.button;
-                        type = "button";
+                        globals[lvl].context.drawImage(images["button_off"],(row * 50),(col * 50));
+                        type = "button_off";
+                    }
+                    else if($.inArray(i,globals[lvl].bomb) != -1){
+                        globals[lvl].context.drawImage(images["bomb"],(row * 50),(col * 50));
+                        type = "bomb";
                     }
                     else{
-                        globals[lvl].context.fillStyle = images.floor;
                         type = "floor";
                     }
-                    globals[lvl].context.fillRect((row * 50),(col * 50), 50, 50);
 
                     if(type == "hero")
                         var object = new hero(row,col,type,"floor");
                     else if(type == "rock")
-                        var object = new movableItem(row,col,type,"floor");
+                        var object = new item(row,col,type,"floor","dropable");
+                    else if(type == "bomb")
+                        var object = new item(row,col,type,"floor","movpable");
                     else
                         var object = new texture(row,col,type);
 
@@ -155,11 +194,11 @@ function get_random_color() {
                 var next_block = globals[current_lvl].entities.cases[next];
 
                 // collision bord
-                if(next_block.type == "bordure" || next_block.type == "toggle_rock_on"){
+                if(next_block.type == "bordure" || next_block.type == "toggle_rock_off"){
                     return;
                 }
                 else if(next_block.type == "rock"){
-                    var next_rock;
+                    var next_rock,prev;
                     if(key == 37){
                         next_rock = next + 12;
                     }
@@ -174,65 +213,108 @@ function get_random_color() {
                     }
                     var next_rock_block = globals[current_lvl].entities.cases[next_rock];
 
-                    if(next_rock_block.type == "bordure" || next_rock_block.type == "rock" || next_rock_block.type == "toggle_rock_on")
+                    if(next_rock_block.type == "bordure" || next_rock_block.type == "rock" || next_rock_block.type == "toggle_rock_off")
                         return;
 
-                    newRock = new movableItem(next_rock_block.x,next_rock_block.y,next_block.type,next_rock_block.type);
-                    console.log(newRock);
-                    globals[current_lvl].context.fillStyle = images.rock;
-                    globals[current_lvl].context.fillRect((next_rock_block.x * 50),(next_rock_block.y * 50), 50, 50);
+                    var type;
+                    prev = next_block.on;
+
+                    globals[current_lvl].context.drawImage(images["floor"],(next_block.x * 50),(next_block.y * 50));
+
+                    if(prev == "toggle_rock_on")
+                        globals[current_lvl].context.drawImage(images["toggle_rock_on"],(next_block.x * 50),(next_block.y * 50));
+
+                    if(next_rock_block.type == "button_off")
+                        type = "button_on";
+                    else
+                        type = next_rock_block.type;
+
+                    newRock = new item(next_rock_block.x,next_rock_block.y,next_block.type,type,prev);
+                    globals[current_lvl].context.drawImage(images["rock"],(next_rock_block.x * 50),(next_rock_block.y * 50));
                     globals[current_lvl].entities.cases[next_rock] = newRock;
                 }
+                else if(next_block.type == "bomb"){
 
-                if(next_block.type == "button" || hero_block.on == "button" || newRock.on == "button"){
+                }
+
+                console.log(next_block);
+                if(next_block.type == "button_off" || hero_block.on == "button_on" || newRock.on == "button_on" || next_block.on == "button_on"){
                     var bool, pos;
-                    if(hero_block.on == "button"){
-                        bool = "on";
+                    if(hero_block.on == "button_on"){
+                        bool = "off";
                         pos = hero;
                     }
-                    else if(newRock.on == "button"){
-                        bool = "off";
+                    else if(newRock.on == "button_on"){
+                        bool = "on";
                         pos = next_rock;
                     }
                     else{
-                        bool = "off";
+                        bool = "on";
                         pos = next;
                     }
+
+                    console.log(pos);
 
                     var i = globals[current_lvl].toggle_rock_link[pos].length - 1;
                     var arr = globals[current_lvl].toggle_rock_link[pos];
                     for(i; i >= 0; i--){
                         var object = globals[current_lvl].entities.cases[arr[i]];
-                        globals[current_lvl].context.fillStyle = images["toggle_rock_" + bool];
-                        globals[current_lvl].context.fillRect((object.x * 50),(object.y * 50), 50, 50);
+                        globals[current_lvl].context.drawImage(images["toggle_rock_" + bool],(object.x * 50),(object.y * 50));
                         globals[current_lvl].entities.cases[arr[i]].type = "toggle_rock_" + bool;
                     }
+
+                    console.log("here");
+
+                    if(newRock.on == "button_on" && bool == "on"){
+                        console.log("brick");
+                        globals[current_lvl].context.drawImage(images["floor"],(next_rock_block.x * 50),(next_rock_block.y * 50));
+                        globals[current_lvl].context.drawImage(images["button_on"],(next_rock_block.x * 50),(next_rock_block.y * 50));
+                        globals[current_lvl].context.drawImage(images["rock"],(next_rock_block.x * 50),(next_rock_block.y * 50));
+
+                    }
+                    else if(bool == "on"){
+                        console.log("seul");
+                        globals[current_lvl].context.drawImage(images["floor"],(next_block.x * 50),(next_block.y * 50));
+                        globals[current_lvl].context.drawImage(images["button_on"],(next_block.x * 50),(next_block.y * 50));
+                    }
+                    else{
+                        console.log("here3");
+                        globals[current_lvl].context.drawImage(images["floor"],(hero_block.x * 50),(hero_block.y * 50));
+                        hero_block.on = "button_off";
+                    }
+
                 }
 
-                // new floor case after move
                 console.log(hero_block);
+                // new floor case after move
                 var floor = new texture(hero_block.x,hero_block.y,hero_block.on);
-                globals[current_lvl].context.fillStyle = images[hero_block.on];
+                globals[current_lvl].context.drawImage(images[hero_block.on],(hero_block.x * 50),(hero_block.y * 50));
                 globals[current_lvl].entities.cases[hero] = floor;
-                globals[current_lvl].context.fillRect((hero_block.x * 50),(hero_block.y * 50), 50, 50);
 
                 //update hero object
                 hero_block.x = next_block.x;
                 hero_block.y = next_block.y;
-                if(next_block.type == "rock" && next_block.on != "button" && next_block.on != "toggle_rock_off"){
+                if(prev == "toggle_rock_on"){
+                    hero_block.on = "toggle_rock_on";
+                }
+                else if(next_block.type == "rock" && next_block.on != "button_off" && next_block.on != "toggle_rock_off" && prev != "button_on"){
                     hero_block.on = "floor";
                 }
-                else if(next_block.on == "button" || next_block.on == "toggle_rock_off"){
+                else if(next_block.on == "button_off" || next_block.on == "toggle_rock_off"){
                     hero_block.on = next_block.on;
+                }
+                else if(next_block.type == "button_off"){
+                    hero_block.on = "button_on";
+                }
+                else if(prev == "button_on"){
+                    hero_block.on = "button_on";
                 }
                 else{
                     hero_block.on = next_block.type;
                 }
 
-
                 // new case of hero
-                globals[current_lvl].context.fillStyle = images.hero;
-                globals[current_lvl].context.fillRect((next_block.x * 50),(next_block.y * 50), 50, 50);
+                globals[current_lvl].context.drawImage(images["hero_bot"],(next_block.x * 50),(next_block.y * 50));
                 globals[current_lvl].entities.cases[next] = hero_block;
 
                 // reset number case of hero
